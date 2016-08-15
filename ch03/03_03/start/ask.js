36b765ea71ace880718806d28ec91616cd4b7463
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-15 09:59:54 +0200

    Played with process.stdin and process.stdout

---
+++ ch03/03_03/start/ask.js
@@ -0,0 +1,31 @@
+/* jshint esversion: 6 */
+var questions = [
+    "What is your name?",
+    "What is your fav hobby?",
+    "What is your preferred programming language?"
+];
+
+var answers = [];
+
+function ask(i) {
+    process.stdout.write(`\n\n\n\n ${questions[i]}`);
+    process.stdout.write("  >  ");
+}
+
+process.stdin.on('data', function(data) {
+    // process.stdout.write('\n' + data.toString().trim() + '\n');
+    answers.push(data.toString().trim());
+    if(answers.length < questions.length) {
+        ask(answers.length); //nice ;)
+    } else {
+        process.exit();
+    }
+});
+
+process.on('exit', function() {
+    process.stdout.write("\n\n\n\n");
+    process.stdout.write(`Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later`);
+    process.stdout.write("\n\n\n\n");
+});
+
+ask(0);
