be2046d7628b83d639a1c71da7e590c9cea5a35b
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-15 14:32:01 +0200

    Writing and appending files

---
+++ ch05/05_03/start/Jon.md
@@ -0,0 +1,5 @@
+Jon
+==================
+
+* Hello, I'm Jon 
+* Never spend the money before you have earned it 
---
+++ ch05/05_03/start/create.js
@@ -0,0 +1,18 @@
+/* jshint esversion: 6 */
+var fs = require('fs');
+
+var md = `
+Sample Markdown Title
+=====================
+
+Sample subtitle
+---------------
+
+* point
+* point
+* point
+`;
+
+fs.writeFile('sample.md', md.trim(),function(err) {
+    console.log("File Created");
+});
