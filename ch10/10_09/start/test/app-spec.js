144710ba6a8f657e9d371cd0a9f73a704005f490
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-16 15:29:10 +0200

    Implemented server response check with Cheerio

--- ch10/10_09/start/test/app-spec.js
+++ ch10/10_09/start/test/app-spec.js
@@ -1,12 +1,18 @@
-var request = require("supertest");
+var request = require('supertest');
 var expect = require('chai').expect;
+var cheerio = require('cheerio');
 var rewire = require('rewire');
 var app = rewire('../app');
 
 describe("Dictionary App", function () {
 
     it("Loads the home page", function(done) {
-        request(app).get("/").expect(200).end(done);
+        request(app).get('/').expect(200).end(function(err, res) {
+            var $ = cheerio.load(res.text);
+            var pageHeading = $("body>h1:first-child").text();
+            expect(pageHeading).to.equal("Skier Dictionary");
+            done();
+        });
     });
 
     describe("Dictionary API", function () {
@@ -28,22 +34,27 @@ describe("Dictionary App", function () {
         });
 
         it("GETS dictionary-api", function(done) {
-            request(app).get("/dictionary-api").expect(200).end(done);
+            var defs = this.defs; // So it doesn't fall out of scope
+            request(app).get('/dictionary-api')
+                .expect(200)
+                .end(function(err, res) {
+                    var terms = JSON.parse(res.text);
+                    expect(terms).to.deep.equal(defs);
+                    done();
+                });
         });
 
         it("POSTS dictionary-api", function(done) {
-            request(app)
-               .post("/dictionary-api")
-               .send({ "term": "Three", "defined": "Term Three Defined"})
-               .expect(200)
-               .end(done);
+            request(app).post('/dictionary-api')
+                .send({"term": "Three", "defined": "Term Three Defined"})
+                .expect(200)
+                .end(done);
         });
 
         it("DELETES dictionary-api", function(done) {
-            request(app)
-               .delete("/dictionary-api/One")
-               .expect(200)
-               .end(done);
+            request(app).delete('/dictionary-api/One')
+                .expect(200)
+                .end(done);
         });
 
     });
