1bfcd2fcd40e7a05df1242389db4ed1dbf0e43bc
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-16 11:28:05 +0200

    Created a websocket server

--- ch09/09_01/start/ws-client.js
+++ ch09/09_01/start/ws-client.js
@@ -1,4 +1,18 @@
 
+var ws = new WebSocket('ws://localhost:3000');
+
+ws.onopen = function() {
+    setTitle("Connected to Cyber Chat");
+};
+
+ws.onclose = function() {
+    setTitle("DISCONNECTED");
+};
+
+ws.onmessage = function(payload) {
+    printMessage(payload.data);
+};
+
 document.forms[0].onsubmit = function () {
     var input = document.getElementById('message');
     input.value = '';
