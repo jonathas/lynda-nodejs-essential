9f9be330274caa4e5270c91e1c7d952f255367a8
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-16 10:03:43 +0200

    Started using Express

---
+++ ch08/08_02/start/app.js
@@ -0,0 +1,17 @@
+/* jshint esnext: true */
+var express = require('express');
+var app = express();
+
+// Logging requests
+app.use(function(req, res, next) {
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+});
+
+app.use(express.static('./public'));
+
+app.listen(3000);
+
+console.log('Express app running on port 3000');
+
+module.exports = app;
