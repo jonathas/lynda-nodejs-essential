a73e012ed2a45b2219128c613d109f96c7102882
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-16 10:43:53 +0200

    Implemented bodyparser, post and delete methods for the API

--- ch08/08_04/start/app.js
+++ ch08/08_04/start/app.js
@@ -1,5 +1,7 @@
-var express = require("express");
-var cors = require("cors");
+/* jshint esnext: true */
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
 
 var skierTerms = [
@@ -17,22 +19,37 @@ var skierTerms = [
     }
 ];
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+// Logging requests
 app.use(function(req, res, next) {
-	console.log(`${req.method} request for '${req.url}'`);
-	next();
+    console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+    next();
 });
 
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 
 app.use(cors());
 
-app.get("/dictionary-api", function(req, res) {
-	res.json(skierTerms);
+app.get('/dictionary-api', function(req,res) {
+    res.json(skierTerms);
+});
+
+app.post('/dictionary-api', function(req,res) {
+    skierTerms.push(req.body);
+    res.json(skierTerms);
+});
+
+app.delete('/dictionary-api/:term', function(req,res) {
+    skierTerms = skierTerms.filter(function(definition) {
+        return definition.term.toLowerCase() !== req.params.term.toLowerCase();
+    });
+    res.json(skierTerms);
 });
 
 app.listen(3000);
 
-console.log("Express app running on port 3000");
+console.log('Express app running on port 3000');
 
 module.exports = app;
