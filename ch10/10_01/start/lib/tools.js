bb854947ebeb680ae5c2cd071526ca75b31e37b7
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-16 12:14:29 +0200

    Added test with Mocha and Chai

---
+++ ch10/10_01/start/lib/tools.js
@@ -0,0 +1,6 @@
+/* jshint esnext: true */
+module.exports = {
+    printName(person) {
+        return `${person.last}, ${person.first}`;
+    }
+};
---
+++ ch10/10_01/start/test/tools-spec.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+var tools = require('../lib/tools');
+
+describe("printName()", function() {
+    it("should print the last name first", function() {
+        var results = tools.printName({first: "Jon", last: "Ribeiro"});
+
+        expect(results).to.equal("Ribeiro, Jon");
+    });
+});
